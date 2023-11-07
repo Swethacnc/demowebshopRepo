12b88b484ce6e5852ed85ba8b7ed935ee589e371
Windows <Windows@momdad>
2023-11-07 11:41:16 +0530

    This is my first commit

---
+++ AdvanceSelenium10-30/src/main/java/genericUtilities/IAutoConstants.java
@@ -0,0 +1,12 @@
+package genericUtilities;
+
+public interface IAutoConstants {
+String PROPERTY_FILE_PATH="./src/test/resources/configurationData.properties";
+String EXCEL_PATH="./src/test/resources/Shopper.xlsx";
+String DATABASE_URL="jdbc:mysql://localhost:3306/qspider";
+String DATABASE_UN="root";
+String DATABASE_PWD="root";
+String BROWSER="chrome";
+
+
+}
