55d98167c592d5b559a3e8b730d81a1df5f952f3
Windows <Windows@momdad>
2023-11-10 13:12:34 +0530

    this is a commit from TE

---
+++ AdvanceSelenium10-30/src/test/java/Home/DemoPractice.java
@@ -0,0 +1,11 @@
+package Home;
+
+import genericUtilities.BaseClass;
+
+public class DemoPractice extends BaseClass{
+	
+	public void LaunchingTheBrowser( ) {
+		driver.navigate().to("https://www.facebook.com/");
+	}
+
+}
