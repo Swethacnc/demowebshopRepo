480c5152e7edc0b3bcd1b13c9355a40095fe12d1
Windows <Windows@momdad>
2023-11-10 13:33:58 +0530

    This commit for practice

--- AdvanceSelenium10-30/src/test/java/Home/DemoPractice2.java
+++ AdvanceSelenium10-30/src/test/java/Home/DemoPractice2.java
@@ -6,7 +6,8 @@ import org.testng.annotations.Test;
 import genericUtilities.BaseClass;
 import genericUtilities.ITestListnerUtility;
 @Listeners(ITestListnerUtility.class)
-public class DemoPractice extends BaseClass{
+
+public class DemoPractice2 extends BaseClass{
 	@Test
 	public void LaunchingTheBrowser( ) {
 		driver.navigate().to("https://www.facebook.com/");
